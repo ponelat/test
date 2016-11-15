a0154f0d9b946f778f4d5977ba685a393e0eefb0
ponelat <jponelat@gmail.com>
2016-11-15 16:48:36 +0000

    Generated code by SwaggerHub

---
+++ go/pet.go
@@ -0,0 +1,20 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Pet struct {
+
+}
+
+func AddPet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func AddPet_1(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
