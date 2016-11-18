0d6ed9c7ddda226eb8b17363da3c4f0c132326ca
root <root@swaggerhub-registry-service-1>
2016-11-18 14:54:39 +0000

    Removed files managed by codegen

--- code/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject this-is-an-org "1"
-  :description "Hello"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]
-                 [cheshire "5.5.0"]])
--- codegen/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject 1 "1"
-  :description "1"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-http "2.0.0"]
-                 [cheshire "5.5.0"]])
