5296cddd902f96546a123b84e16bad47cf5747b4
ponelat <jponelat@gmail.com>
2016-11-18 16:07:34 +0000

    Generated code by SwaggerHub

--- slim/index.php
+++ slim/index.php
@@ -1,28 +1,12 @@
 <?php
 /**
- * Other Document
+ * Uber API
  * @version base
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Slim\App();
-
-
-/**
- * POST addPet
- * Summary: Add a new pet to the store
- * Notes: 
-
- */
-$app->POST('/dogs', function($request, $response, $args) {
-            
-            
-            
-            
-            $response->write('How about implementing addPet as a POST method ?');
-            return $response;
-            });
 
 
 
